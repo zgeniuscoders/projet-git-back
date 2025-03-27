97902fc82ee1add98074ffce4a100946fb8c3c7d
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2025-03-27 14:49:49 +0100

    Ajout de la fonction d'abonnement (newsletter)

--- app/Models/Newsletter.php
+++ app/Models/Newsletter.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Newsletter extends Model
 {
     use HasFactory;
-    protected $fillable = ['email'];
+    protected $fillable = ['email']; 
 }
--- database/migrations/2025_03_27_133121_create_newsletters_table.php
+++ database/migrations/2025_03_27_133121_create_newsletters_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('newsletters', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
