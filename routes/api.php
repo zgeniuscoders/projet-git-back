8c661ff4b400ba1ae5480c18e6333eb2c59a158c
zgeniuscoders <zgeniuscoders@gmail.com>
2025-03-28 12:41:34 +0100

    resolution confilt dans le fichier api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ArticleLikeController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\NewsletterController;
