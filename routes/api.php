4b833ea21eaeeef6729a4d8efcd85c65fa3beabb
zgeniuscoders <zgeniuscoders@gmail.com>
2025-03-28 12:28:47 +0100

    protection de la route pour  like avec le middleware d'authentification

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ArticleLikeController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommentController;
