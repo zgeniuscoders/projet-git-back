e89c8bfb3ade6c48ee5dcdeee10535f618c9b212
zgeniuscoders <zgeniuscoders@gmail.com>
2025-03-28 10:59:49 +0100

    Implémentation de fonctionnalité pour aimer un article
    # Conflicts:
    #	routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ArticleLikeController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\NewsletterController;
@@ -29,14 +30,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route securisée
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-
+    Route::post('likes', ArticleLikeController::class);
 });
 
 Route::apiResource('/articles', ArticleController::class);
 Route::apiResource('/categories', CategoryController::class);
 Route::apiResource('/comments', CommentController::class);
-Route::apiResource('/newsletter', NewsletterController::class);
-
+Route::apiResource('/newsletter' , NewsletterController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
